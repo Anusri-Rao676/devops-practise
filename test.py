eb654da999ab50d58ee3b361204e3a4f9caae181
Anusri Rao <anusriraok@gmail.com>
2025-11-04 15:04:15 +0530

    Refactor test to call out() directly

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from app3 import out
 class MyTests(unittest.TestCase):
 
   def test_addition(self):
-    result = out()
-    self.assertEqual(result,'heyy')
+    self.assertEqual(out(),'heyy')
 if __name__ == '__main__':
   unittest.main()
