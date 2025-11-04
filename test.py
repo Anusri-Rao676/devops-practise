c5cee430c5a5a2a60f0244f4a5f18d71c584878e
Anusri Rao <anusriraok@gmail.com>
2025-11-04 15:01:41 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import unittest
 from app3 import out
 class MyTests(unittest.TestCase):
-  result = out()
+
   def test_addition(self):
+    result = out()
     self.assertEqual(result,"heyy")
 if __name__ == '__main__':
   unittest.main()
